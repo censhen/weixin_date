8fda5a48487659ae146e928a728adef94b62c0a0
shencen <censhen17@gmail.com>
2016-05-22 10:15:59 +0800

    test

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -156,7 +156,35 @@ class WechatController extends Controller {
             ],
         ];
 
-        $wechat->menu->add($raw_menu);
+        $buttons = [
+            [
+                "type" => "click",
+                "name" => "今日歌曲",
+                "key"  => "V1001_TODAY_MUSIC"
+            ],
+            [
+                "name"       => "菜单",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "搜索",
+                        "url"  => "http://www.soso.com/"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "视频",
+                        "url"  => "http://v.qq.com/"
+                    ],
+                    [
+                        "type" => "click",
+                        "name" => "赞一下我们",
+                        "key" => "V1001_GOOD"
+                    ],
+                ],
+            ],
+        ];
+
+        $wechat->menu->add($buttons);
 
         return "自定义菜单成功";
     }
